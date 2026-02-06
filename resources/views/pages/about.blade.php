cedcdc84938006be851f69a98d21e333d381c20b
kdabal <kdabal1997@gmail.com>
2026-02-06 13:21:03 +0100

    layout change

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,17 +3,20 @@
 @section('content')
 <div class="container d-flex justify-content-center mt-5">
     <div class="d-flex flex-column align-items-center gap-2">
-        <p>
-            MCdle is a fan-made project created for educational purposes.
-        This project is not affiliated with or endorsed by Mojang Studios or Microsoft.
-        The game is free to play and was developed purely for fun and learning.
-        Minecraft is a registered trademark of Microsoft.
-        </p>
-
-        <p>
-            Special thanks to Mojang for creating such an amazing game,
-        and to the Minecraft Wiki community for providing valuable information about the game.
-        </p>
+        <div class="guess_panel">
+            <div class="guess_content">
+                <p>
+                    MCdle is a fan-made project created for educational purposes.
+                This project is not affiliated with or endorsed by Mojang Studios or Microsoft.
+                The game is free to play and was developed purely for fun and learning.
+                Minecraft is a registered trademark of Microsoft.
+                </p>
+                <p>
+                    Special thanks to Mojang for creating such an amazing game,
+                and to the Minecraft Wiki community for providing valuable information about the game.
+                </p>
+            </div>
+        </div>
     </div>
 </div>
 @endsection
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -4,16 +4,21 @@
 <div class="container d-flex justify-content-center mt-5">
     <div class="d-flex flex-column align-items-center gap-2">
         <a href="{{ route('about') }}">about</a>
-        <a href="{{ route('mob_guesser') }}">
-            <button class="minecraft-btn mx-auto text-center text-white truncate p-1 border-2 border-b-4 hover:text-yellow-200">Mobs</button>
-        </a>
-        <a href="">
-            <button class="minecraft-btn mx-auto text-center text-white truncate p-1 border-2 border-b-4 hover:text-yellow-200">Blocks<p>(Work in progress)</p></button>
-        </a>
-        
-        <a href="">
-            <button class="minecraft-btn mx-auto text-center text-white truncate p-1 border-2 border-b-4 hover:text-yellow-200">Items<p>(Work in progress)</p></button>
-        </a>
+        <div class="minecraft-btn-outside">
+            <a href="{{ route('mob_guesser') }}">
+                <button class="minecraft-btn mx-auto text-center text-white truncate hover:text-yellow-200">Mobs</button>
+            </a>
+        </div>
+        <div class="minecraft-btn-outside">
+            <a href="{{ route('mob_guesser') }}">
+                <button class="minecraft-btn mx-auto text-center text-white truncate hover:text-yellow-200">Blocks<p>(Work in progress)</button>
+            </a>
+        </div>
+        <div class="minecraft-btn-outside">
+            <a href="{{ route('mob_guesser') }}">
+                <button class="minecraft-btn mx-auto text-center text-white truncate hover:text-yellow-200">Items<p>(Work in progress)</button>
+            </a>
+        </div>
         
     </div>
 </div>
